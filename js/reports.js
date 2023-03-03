71efe253bab8cc58f2d84e8121c3e54b2827f3a6
Ghanshyam Katriya <ghanshyam.katriya@spearline.com>
2023-03-03 11:25:31 +0530

    Assets Stuff

---
+++ js/reports.js
@@ -0,0 +1,56 @@
+var apiUrl = "http://localhost:3000";
+var urlParams = new URLSearchParams(window.location.search); //get all parameters
+var report_id = urlParams.get('report_id'); //extract the foo parameter - this will return NULL if foo isn't a parameter
+
+if(report_id) { //check if foo parameter is set to anything
+  $.ajax({
+      url: apiUrl+"/report/"+report_id,
+      method: "GET",
+      success: function(response) {
+      let _tableData = "";
+      if(response.data) {
+          _tableData += "<tr>\
+          <td>"+response.data.report_id+"</td>\
+          <td>"+response.data.report_file_id+"</td>\
+          <td>"+response.data.report_status+"</td>\
+          <td><a href='"+response.data.aws_file_path+"' target='_blank'>Download</a></td>\
+          <td><a href='reports.html?report_id="+response.data.report_file_id+"'><img src='images/eye-solid.svg' height='24px'></a></td>\
+          </tr>";
+      } else {
+        _tableData += "No Record Found.";
+      }
+      $('._tableData').html(_tableData);    
+    },
+    error: function(xhr, status, error) {
+      console.log("Error:", error);
+    }
+  });
+} else {
+    // NOTE: Send Data to REST API
+    $.ajax({
+      url: apiUrl+"/reports",
+      method: "GET",
+      success: function(response) {
+      // console.log("Success:", response);
+      let _tableData = "";
+      if(response.data.length > 0) {
+        for(let i = 0; i < response.data.length; i++) {
+          _tableData += "<tr>\
+          <td>"+response.data[i].report_id+"</td>\
+          <td>"+response.data[i].report_file_id+"</td>\
+          <td>"+response.data[i].report_status+"</td>\
+          <td><a href='"+response.data[i].aws_file_path+"' target='_blank'>Download</a></td>\
+          <td><a href='reports.html?report_id="+response.data[i].report_file_id+"'><img src='images/eye-solid.svg' height='24px'></a></td>\
+          </tr>";
+        }  
+      } else {
+        _tableData += "No Record Found.";
+      }
+      $('._tableData').html(_tableData);    
+    },
+    error: function(xhr, status, error) {
+      console.log("Error:", error);
+    }
+  });
+  }
+
