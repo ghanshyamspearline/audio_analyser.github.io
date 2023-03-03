56ec2edcc6860b1ec1cba400b2b3a55d6779c783
Ghanshyam Katriya <ghanshyam.katriya@spearline.com>
2023-03-03 13:57:58 +0530

    Added REST API Integration to execute add record endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var _config = null;
 var s3;
 var bucketName = "audio-analyzer";
 var bucketRegion = "ap-south-1";
+var API_URL = "http://localhost:3000";
 document.addEventListener("DOMContentLoaded", function () {
   console.log("document is ready. I can sleep now");
   AWS.config.update({
@@ -22,7 +23,7 @@ const analyzeAudio = () => {
 const makeid = (length) => {
   let result = "";
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
@@ -40,42 +41,55 @@ const uploadAudio = () => {
     var _fileId = makeid(10);
 
     var fileName =
-      _timestamp + "_" + _fileId + "." + file.name.split(".").pop();
+    _timestamp + "_" + _fileId + "." + file.name.split(".").pop();
 
     var fileUrl = "https://" + bucketName + ".s3." + bucketRegion + ".amazonaws.com/" + fileName;
     $(".btn").hide();
     $("progress").show();
     console.log("File will be available at location " + fileUrl);
     s3.upload(
-      {
-        Key: fileName,
-        Body: file,
-        ACL: "public-read",
-      },
-      function (err, data) {
-        if (err) {
-          console.error("Error uploading :", err, data);
-        } else {
-          $(".status").html(
-            "<span class='text-success'>Successfully Uploaded!</span><br><h6>Keep tracking number "+_fileId+" for your refrences.</h6>"
+    {
+      Key: fileName,
+      Body: file,
+      ACL: "public-read",
+    },
+    function (err, data) {
+      if (err) {
+        console.error("Error uploading :", err, data);
+      } else {
+        $(".status").html(
+          "<span class='text-success'>Successfully Uploaded!</span><br><h6>Keep tracking number "+_fileId+" for your refrences.</h6>"
           );
-          $("progress").hide();
-          $(".btn").show();
-          $("#_audioFile").val("");
+        $("progress").hide();
+        $(".btn").show();
+        $("#_audioFile").val("");
+
+          // Execute REST API to insert records into database
+        $.ajax({
+          url: API_URL+"/report",
+          method: "POST",
+          data: {report_file_name: file.name, aws_file_path: fileUrl, report_file_id: _fileId},
+          success: function(response) {
+            console.log("Success:", response);
+          },
+          error: function(xhr, status, error) {
+            console.log("Error:", error);
+          }
+        });
         }
       }
-    ).on("httpUploadProgress", function (progress) {
-      var uploaded = parseInt((progress.loaded * 100) / progress.total);
-      $("progress").attr("value", uploaded);
-    });
-  }
-};
+      ).on("httpUploadProgress", function (progress) {
+        var uploaded = parseInt((progress.loaded * 100) / progress.total);
+        $("progress").attr("value", uploaded);
+      });
+    }
+  };
 
-const viewReports = () => {
-  $("#_config").hide();
-  $("#_reports").show();
-};
-const backToTool = () => {
-  $("#_reports").hide();
-  $("#_config").show();
-};
+  const viewReports = () => {
+    $("#_config").hide();
+    $("#_reports").show();
+  };
+  const backToTool = () => {
+    $("#_reports").hide();
+    $("#_config").show();
+  };
--- js/reports.js
+++ js/reports.js
@@ -9,9 +9,13 @@ if(report_id) { //check if foo parameter is set to anything
       success: function(response) {
       let _tableData = "";
       if(response.data) {
+          const _localDate = new Date(response.data.report_created_time);
+          const dateString = _localDate.toLocaleDateString();
+          const timeString = _localDate.toLocaleTimeString();
           _tableData += "<tr>\
           <td>"+response.data.report_id+"</td>\
           <td>"+response.data.report_file_id+"</td>\
+          <td>"+dateString+ " "+timeString+"</td>\
           <td>"+response.data.report_status+"</td>\
           <td><a href='"+response.data.aws_file_path+"' target='_blank'>Download</a></td>\
           <td><a href='reports.html?report_id="+response.data.report_file_id+"'><img src='images/eye-solid.svg' height='24px'></a></td>\
@@ -35,9 +39,13 @@ if(report_id) { //check if foo parameter is set to anything
       let _tableData = "";
       if(response.data.length > 0) {
         for(let i = 0; i < response.data.length; i++) {
+          const _localDate = new Date(response.data[i].report_created_time);
+          const dateString = _localDate.toLocaleDateString();
+          const timeString = _localDate.toLocaleTimeString();
           _tableData += "<tr>\
           <td>"+response.data[i].report_id+"</td>\
           <td>"+response.data[i].report_file_id+"</td>\
+          <td>"+dateString+ " "+timeString+"</td>\
           <td>"+response.data[i].report_status+"</td>\
           <td><a href='"+response.data[i].aws_file_path+"' target='_blank'>Download</a></td>\
           <td><a href='reports.html?report_id="+response.data[i].report_file_id+"'><img src='images/eye-solid.svg' height='24px'></a></td>\
